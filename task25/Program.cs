fc5e3776eb5f8b42c8c4af5c0fcab7fe3ebe92cb
Sergey <windisel1251@gmail.com>
2023-01-25 00:46:48 +1200

    Update

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetAnsw(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 0; i < numB; i++)
+        result = result * numA;
+    return result;
+}
+
+Console.Write("Введите число А: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine($"{numA}, {numB} -> {GetAnsw(numA, numB)}");
---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSumm(string inBox)
+{
+    int num = int.Parse(inBox);
+    int result = 0;
+    for (int i = 0; i < inBox.Length; i++)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+string box = Console.ReadLine();
+Console.WriteLine($"{box} -> {GetSumm(box)}");
